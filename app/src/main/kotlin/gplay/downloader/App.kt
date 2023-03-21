e847467daf377e51ab841c80f0467f1cbbcc15fd
İlker Avcı <ikolomiko@gmail.com>
2023-03-21 17:28:03 +0300

    app: Use the new ConfigManager
    Also use the proper exit code (2) for incorrect number of arguments

--- app/src/main/kotlin/gplay/downloader/App.kt
+++ app/src/main/kotlin/gplay/downloader/App.kt
@@ -1,11 +1,7 @@
 package gplay.downloader
 
-import com.aurora.gplayapi.helpers.AuthHelper
-import java.io.BufferedReader
 import java.io.File
 import java.io.InputStream
-
-
 
 fun readAppIds(path: String): List<String> {
     val ls: MutableList<String> = mutableListOf()
@@ -25,7 +21,7 @@ fun main(args: Array<String>) {
 
     if (args.size != 2) {
         println("usage: gplay-downloader <appids file> <output path>")
-        return
+        System.exit(2)
     }
 
     // Create output dir
@@ -38,13 +34,13 @@ fun main(args: Array<String>) {
     log.status("=====Initialized=====")
 
     // Read auth configuration and login
-    val (email, aasToken) = readAuthConfig()
-    val authData = AuthHelper.build(email, aasToken)
+    /// TODO replace placeholders below
+    val configManager = ConfigManager("authConfigPath", "proxyConfigPath")
 
-    log.status("Logged in")
+    log.status("Initialized config manager")
 
     // Download each app
-    val downloader = Downloader(log, outputPath, authData)
+    val downloader = Downloader(log, outputPath, configManager)
     downloader.downloadAll(appIds)
 
     log.status("=====Finished=====")
