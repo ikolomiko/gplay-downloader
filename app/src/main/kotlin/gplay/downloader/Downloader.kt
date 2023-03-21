4dd7a797b71c91ff4f00bdf6fb3095e623a81965
İlker Avcı <ikolomiko@gmail.com>
2023-03-21 17:25:36 +0300

    downloader: Add support for proxies and the new ConfigManager

--- app/src/main/kotlin/gplay/downloader/Downloader.kt
+++ app/src/main/kotlin/gplay/downloader/Downloader.kt
@@ -2,6 +2,7 @@ package gplay.downloader
 
 import com.aurora.gplayapi.data.models.AuthData
 import com.aurora.gplayapi.helpers.AppDetailsHelper
+import com.aurora.gplayapi.helpers.AuthHelper
 import com.aurora.gplayapi.helpers.PurchaseHelper
 import java.io.File
 import java.net.URL
@@ -11,7 +12,7 @@ import java.nio.file.Paths
 class Downloader(
         val log: Logger,
         val outputPath: String,
-        val authData: AuthData,
+        val configManager: ConfigManager,
 ) {
     data class Query(val url: String, val appId: String, val isFree: Boolean)
 
@@ -50,16 +51,44 @@ class Downloader(
         URL(query.url).openStream().use { Files.copy(it, Paths.get(path)) }
     }
 
+    private fun applyProxyConfig(proxyConfig: ProxyConfig) {
+        val (proxyHost, proxyPort, proxyUser, proxyPassword) = proxyConfig
+        log.status("Using proxy ${proxyHost}:${proxyPort}")
+
+        System.setProperty("http.proxyHost", proxyHost)
+        System.setProperty("http.proxyPort", proxyPort)
+        System.setProperty("http.proxyUser", proxyUser.orEmpty())
+        System.setProperty("http.proxyPassword", proxyPassword.orEmpty())
+
+        System.setProperty("https.proxyHost", proxyHost)
+        System.setProperty("https.proxyPort", proxyPort)
+        System.setProperty("https.proxyUser", proxyUser.orEmpty())
+        System.setProperty("https.proxyPassword", proxyPassword.orEmpty())
+    }
+
     public fun downloadAll(appIds: List<String>) {
         val len = appIds.size
-        appIds.forEachIndexed { index, id ->
+        for ((index, id) in appIds.withIndex()) {
             log.status("Getting ${index+1}/$len AppId $id")
 
+            // Check if the app is already downloaded
             if (File(outputPath + id + ".apk").exists()) {
                 log.info("Already downloaded AppId $id")
-                return@forEachIndexed
+                continue
             }
 
+            // Get downloader config and apply proxy config if given
+            log.status("Getting next downloader config")
+            val (authConfig, proxyConfig) = configManager.getNextConfig()
+            if (proxyConfig != null) {
+                applyProxyConfig(proxyConfig)
+            }
+
+            // Build login credentials
+            val authData = AuthHelper.build(authConfig.email, authConfig.aasToken)
+            log.status("Logged in as ${authConfig.email}")
+
+            // Try to find the app and download
             val query = getQuery(id, authData, log)
             if (query == null) {
                 log.warning("NOTFOUND AppId $id")
